3f4819b8d728bfe7ce812fdabd2f66b6bb112a33
blackninja81 <johnkaburu81@gmail.com>
2022-06-21 12:04:54 +0300

    added target = _blank property

--- src/components/movies.js
+++ src/components/movies.js
@@ -7,7 +7,7 @@ const IMG_API = "https://image.tmdb.org/t/p/original/";
 const Movie = ({id, poster_path}) => (
   
   <div className="movies">
-    <Link to={`/movies/${id}`}>
+    <Link to={`/movies/${id}`} target="_blank">
     <div className="movie">
       <div className="movie_poster">
         <img src={IMG_API + poster_path} alt="No Preview Available"></img>
